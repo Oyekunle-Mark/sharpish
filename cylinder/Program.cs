b07d8db8a40cc1b4561523b759f3b1494d6c6f9a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 17:47:50 +0100

    Gets radius and height

--- cylinder/Program.cs
+++ cylinder/Program.cs
@@ -6,7 +6,17 @@ namespace cylinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cylinder application 1.0!");
+
+            Console.Write("Enter the radius of the cylinder: ");
+            string radiusAsString = Console.ReadLine();
+            double radius = Convert.ToDouble(radiusAsString);
+
+            Console.Write("Enter the height of the cylinder: ");
+            string heightAsString = Console.ReadLine();
+            double height = Convert.ToDouble(heightAsString);
+
+            Console.WriteLine("You entered: " + radius + " and " + height);
         }
     }
 }
