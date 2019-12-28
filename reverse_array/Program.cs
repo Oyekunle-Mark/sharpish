daa9aaee6530d37e8f8efbcb44ad60b476659cf9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 18:26:53 +0100

    Stubbed out a solution

--- reverse_array/Program.cs
+++ reverse_array/Program.cs
@@ -7,8 +7,11 @@ namespace reverse_array
         static void Main(string[] args)
         {
             // call generate numbers to get array
+            int[] numberArray = GenerateNumber();
             // pass the number to reverse number
+            ReverseArray(numberArray);
             // print number
+            PrintArray(numberArray);
         }
     }
 }
