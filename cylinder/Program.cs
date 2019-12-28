e86d5e142dddd95025ac96ec2ef73361128ef81a
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 17:43:48 +0100

    Finds the area and volume of a cylinder

---
+++ cylinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cylinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
