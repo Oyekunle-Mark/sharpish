03a042a230f91c4aa6d9d93ff6e9c71c9473c5fc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 18:06:35 +0100

    Got imput

--- basic-calculator/Program.cs
+++ basic-calculator/Program.cs
@@ -6,7 +6,23 @@ namespace basic_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Basic Calculator 1.0\n");
+
+            // get first operand
+            Console.Write("Enter first number: ");
+            string firstOperandString = Console.ReadLine();
+            double firstOperand = Convert.ToDouble(firstOperandString);
+
+            // get second operand
+            Console.Write("Enter second number: ");
+            string secondOperandString = Console.ReadLine();
+            double secondOperand = Convert.ToDouble(secondOperandString);
+
+            // get the operator
+            Console.Write("Enter operator: ");
+            string operation = Console.ReadLine();
+            
+            Console.WriteLine("Operation is " + firstOperand + operation + secondOperand);
         }
     }
 }
