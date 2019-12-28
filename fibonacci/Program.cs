6db69f6c8f302c2af3ee0aab9d9e8781194f871e
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 18:48:36 +0100

    Stubbed out a path

--- fibonacci/Program.cs
+++ fibonacci/Program.cs
@@ -7,6 +7,13 @@ namespace fibonacci
         static void Main(string[] args)
         {
             Console.WriteLine("This program gets an input from the user and prints out the number at that position in the Fibonacci sequence!\n");
+
+            Console.Write("Enter the number: ");
+            string numberAsString = Console.ReadLine();
+            int number = Convert.ToInt32(numberAsString);
+
+            int fibonacciNumber = GetFibonacciNumber(number);
+            Console.WriteLine($"The {number}nth number is {fibonacciNumber}");
         }
     }
 }
