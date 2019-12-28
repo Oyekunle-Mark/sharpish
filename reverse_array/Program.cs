642554dc9e450b973f79c83a7ad5d758b54ff9f6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-28 18:29:58 +0100

    completed generate numbers function

--- reverse_array/Program.cs
+++ reverse_array/Program.cs
@@ -7,11 +7,21 @@ namespace reverse_array
         static void Main(string[] args)
         {
             // call generate numbers to get array
-            int[] numberArray = GenerateNumber();
+            int[] numberArray = GenerateNumbers(10);
             // pass the number to reverse number
             ReverseArray(numberArray);
             // print number
             PrintArray(numberArray);
+        }
+
+        static int[] GenerateNumbers(int length) {
+            int[] numbers = new int[length];
+
+            for (int i = 0; i < length; i++) {
+                numbers[i] = i;
+            }
+
+            return numbers;
         }
     }
 }
