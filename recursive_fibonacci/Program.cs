0073c720aae8b3fef1e8336238cc07728dbd7553
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-29 18:15:55 +0100

    Formatted the code

--- recursive_fibonacci/Program.cs
+++ recursive_fibonacci/Program.cs
@@ -8,18 +8,22 @@ namespace recursive_fibonacci
         {
             // returns the nth fibonacci number from the fibonacci sequence using recursion
             Console.WriteLine("This program prints the nth Fibonacci Number from the Fibonacci sequence!\n");
+
             Console.Write("Enter the nth Fibonacci number to print: ");
             string inputString = Console.ReadLine();
             int inputNumber = Convert.ToInt32(inputString);
+
             // will hold the nth fibonacci number
             int answer;
             // create an array of length inputNumber 
             int[] fibNumbers = new int[inputNumber];
+
             //and add the first and second number
             fibNumbers[0] = 1;
             fibNumbers[1] = 1;
+
             // check if the inputNumber is 1 or 2
-            if(inputNumber == 1 || inputNumber == 2)
+            if (inputNumber == 1 || inputNumber == 2)
                 // set answer to 1
                 answer = 1;
             // nthNumber will the result of calling FindFibonacciNumber
@@ -35,8 +39,10 @@ namespace recursive_fibonacci
             if (current == position - 1)
                 // return sum of the last two numbers in fibNumbers
                 return fibNumbers[current - 1] + fibNumbers[current - 2];
+
             // otherwise, set fibNumbers at current to sum of the last two numbers in fibNumbers
             fibNumbers[current] = fibNumbers[current - 1] + fibNumbers[current - 2];
+
             // make a recursive call to FindFibonacciNumber
             // passing fibNumbers, position and current + 1
             return FindFibonacciNumber(fibNumbers, position, current + 1);
