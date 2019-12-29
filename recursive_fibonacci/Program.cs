f0d841f618b56272dfe14c1676d5dc0d036d77fe
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-29 18:05:24 +0100

    Added a base case

--- recursive_fibonacci/Program.cs
+++ recursive_fibonacci/Program.cs
@@ -7,9 +7,15 @@ namespace recursive_fibonacci
         static void Main(string[] args)
         {
             // returns the nth fibonacci number from the fibonacci sequence using recursion
-            // check if the number is 1 or 2
-            // return 1
+            // check if the inputNumber is 1 or 2
+            if(inputNumber == 1 || inputNumber == 2)
+                // set answer to 1
+                answer = 1;
             // nthNumber will the result of calling FindFibonacciNumber
+            else
+                answer = FindFibonacciNumber(fibNumbers, inputNumber, 2);
+
+            Console.WriteLine($"The {inputNumber}th Fibonacci number is: {answer}");
         }
 
         static int FindFibonacciNumber(int[] fibNumbers, int position, int current)
