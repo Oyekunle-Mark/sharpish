acc446ded18fc0ce4b27d3218ac212ab01cb1093
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-29 18:02:07 +0100

    Implemented fib

--- recursive_fibonacci/Program.cs
+++ recursive_fibonacci/Program.cs
@@ -8,16 +8,21 @@ namespace recursive_fibonacci
         {
             // returns the nth fibonacci number from the fibonacci sequence using recursion
             // check if the number is 1 or 2
-                // return 1
+            // return 1
             // nthNumber will the result of calling FindFibonacciNumber
         }
 
-        static int FindFibonacciNumber(int[] fibNumbers, int position, int current) {
+        static int FindFibonacciNumber(int[] fibNumbers, int position, int current)
+        {
             // check if current equals position - 1
-                // return fibNumbers[current - 1] + fibNumbers[current - 2]
-            // otherwise, set fibNumbers at current to ibNumbers[current - 1] + fibNumbers[current - 2]
+            if (current == position - 1)
+                // return sum of the last two numbers in fibNumbers
+                return fibNumbers[current - 1] + fibNumbers[current - 2];
+            // otherwise, set fibNumbers at current to sum of the last two numbers in fibNumbers
+            fibNumbers[current] = fibNumbers[current - 1] + fibNumbers[current - 2];
             // make a recursive call to FindFibonacciNumber
             // passing fibNumbers, position and current + 1
+            return FindFibonacciNumber(fibNumbers, position, current + 1);
         }
     }
 }
