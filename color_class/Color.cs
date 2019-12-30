91ae180239732eebc106f6a5d4a75a0a4f7692e1
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:31:16 +0100

    Color instance variable and constructors

--- color_class/Color.cs
+++ color_class/Color.cs
@@ -2,8 +2,18 @@ using System;
 
 namespace color_class
 {
-    class Color 
+    class Color
     {
-        
+        private int red;
+        private int green;
+        private int blue;
+        private int alpha;
+
+        public Color(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
     }
 }
