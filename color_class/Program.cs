229592b821c30ae9e9c81f383e44d8e283a4daec
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:46:08 +0100

    Used the Color class

--- color_class/Program.cs
+++ color_class/Program.cs
@@ -6,7 +6,13 @@ namespace color_class
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("The color class.\n");
+
+            Color color = new Color(129, 232, 45);
+            color.SetBlue(0);
+
+            Console.WriteLine($"The color properties are {color.GetRed()}R {color.GetGreen()}G {color.GetBlue()}B {color.GetAlpha()}A");
+            Console.WriteLine($"The grayscale is {color.FindGrayscale()}");
         }
     }
 }
