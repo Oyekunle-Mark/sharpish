f9d3002fa66674a54e2ead3f720fa1acf432cfaf
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:57:08 +0100

    Refacored to use the properties instead

--- color_class/Program.cs
+++ color_class/Program.cs
@@ -9,9 +9,8 @@ namespace color_class
             Console.WriteLine("The color class.\n");
 
             Color color = new Color(129, 232, 45);
-            color.SetBlue(0);
 
-            Console.WriteLine($"The color properties are {color.GetRed()}R {color.GetGreen()}G {color.GetBlue()}B {color.GetAlpha()}A");
+            Console.WriteLine($"The color properties are {color.Red}R {color.Green}G {color.Blue}B {color.Alpha}A");
             Console.WriteLine($"The grayscale is {color.FindGrayscale()}");
         }
     }
