f11dc827cc7837b73624ca4d14e1bffcdfe28dd4
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:53:34 +0100

    Converted alpha to auto-implemented property

--- color_class/Color.cs
+++ color_class/Color.cs
@@ -7,22 +7,13 @@ namespace color_class
         public byte Red { get; set; }
         public byte Green { get; set; }
         public byte Blue { get; set; }
-        public byte Alpha { get; set; }
+        public byte Alpha { get; set; } = 255;
 
         public Color(byte red, byte green, byte blue)
         {
             this.red = red;
             this.green = green;
             this.blue = blue;
-            this.alpha = 255;
-        }
-
-        public Color(byte red, byte green, byte blue, byte alpha)
-        {
-            this.red = red;
-            this.green = green;
-            this.blue = blue;
-            this.alpha = alpha;
         }
 
         public int FindGrayscale()
