d47c49909349fc53686921c5a2c7e97b4031167c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:58:24 +0100

    Refactored to a auto-implemented instances

--- color_class/Color.cs
+++ color_class/Color.cs
@@ -9,13 +9,6 @@ namespace color_class
         public byte Blue { get; set; }
         public byte Alpha { get; set; } = 255;
 
-        public Color(byte red, byte green, byte blue)
-        {
-            Red = red;
-            Green = green;
-            Blue = blue;
-        }
-
         public int FindGrayscale()
         {
             return (Red + Green + Blue) / 3;
