3637fbebbaf7a75cdebfc455779ec1c4e02a94cb
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-30 14:59:39 +0100

    Used object initializer syntax

--- color_class/Program.cs
+++ color_class/Program.cs
@@ -8,7 +8,7 @@ namespace color_class
         {
             Console.WriteLine("The color class.\n");
 
-            Color color = new Color(129, 232, 45);
+            Color color = new Color() { Red = 23, Green = 232, Blue = 023 };
 
             Console.WriteLine($"The color properties are {color.Red}R {color.Green}G {color.Blue}B {color.Alpha}A");
             Console.WriteLine($"The grayscale is {color.FindGrayscale()}");
