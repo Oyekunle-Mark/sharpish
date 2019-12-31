3adeb8baab4d0606c32f3544d960c0bb7c087846
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-12-31 20:32:21 +0100

    Fins the grayscale of a number

--- color_class/Color.cs
+++ color_class/Color.cs
@@ -9,6 +9,10 @@ namespace color_class
         public byte Blue { get; set; }
         public byte Alpha { get; set; } = 255;
 
+        /// <summary>
+        /// Finds the grayscale of a color
+        /// </summary>
+        /// <returns>The grayscale computed</returns>
         public int FindGrayscale()
         {
             return (Red + Green + Blue) / 3;
