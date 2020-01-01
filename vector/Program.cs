1d42584d82afc13740b05244703d1e755e1309f8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 09:35:54 +0100

    Tested the operators

--- vector/Program.cs
+++ vector/Program.cs
@@ -6,7 +6,10 @@ namespace vector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector v1 = new Vector(2, 3, 2);
+            Vector v2 = new Vector(3, 5, 9);
+
+            Vector newVector = v1 + v2;
         }
     }
 }
