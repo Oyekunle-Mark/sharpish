f5d6dd518647af0f8889871522a09c01007cad93
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 10:06:38 +0100

    Used lambda expression instead

--- extension_method/StringExtenstion.cs
+++ extension_method/StringExtenstion.cs
@@ -9,9 +9,7 @@ namespace Extensions
         /// </summary>
         /// <param name="text">The text to be counted</param>
         /// <returns>The number of words.</returns>
-        public static int CountWords(this string text)
-        {
-            return text.Split(' ').Length;
-        }
+        public static int CountWords(this string text) => text.Split(' ').Length;
+    
     }
 }
