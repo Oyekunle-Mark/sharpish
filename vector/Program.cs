76996c75c130e61312ac75e77149ddb8c720c6a3
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 09:22:01 +0100

    Creates a 3D vector class

---
+++ vector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
