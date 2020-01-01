d1236ff7de96ddf1dadc4915f1a5bde294959849
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 10:09:07 +0100

    Converted the vector class method to lambda expressions

--- vector/Vector.cs
+++ vector/Vector.cs
@@ -15,37 +15,20 @@ namespace vector
             Z = z;
         }
 
-        public static Vector operator +(Vector v1, Vector v2)
-        {
-            return new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
-        }
+        public static Vector operator +(Vector v1, Vector v2) => new Vector(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z);
 
-        public static Vector operator -(Vector v1, Vector v2)
-        {
-            return new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
-        }
+        public static Vector operator -(Vector v1, Vector v2) => new Vector(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z);
 
-        public static Vector operator !(Vector v)
-        {
-            return new Vector(-v.X, -v.Y, -v.Z);
-        }
+        public static Vector operator !(Vector v) => new Vector(-v.X, -v.Y, -v.Z);
 
-        public static Vector operator *(Vector v, double scalar)
-        {
-            return new Vector(v.X * scalar, v.Y * scalar, v.Z * scalar);
-        }
+        public static Vector operator *(Vector v, double scalar) => new Vector(v.X * scalar, v.Y * scalar, v.Z * scalar);
 
-        public static Vector operator /(Vector v, double scalar)
-        {
-            return new Vector(v.X / scalar, v.Y / scalar, v.Z / scalar);
-        }
+        public static Vector operator /(Vector v, double scalar) => new Vector(v.X / scalar, v.Y / scalar, v.Z / scalar);
 
         /// <summary>
         /// Pretty prints the values of the Vector instance variables
         /// </summary>
-        public void PrintVector()
-        {
-            Console.WriteLine($"(X: {X}, Y: {Y}, Z: {Z})");
-        }
+        public void PrintVector() => Console.WriteLine($"(X: {X}, Y: {Y}, Z: {Z})");
+
     }
 }
