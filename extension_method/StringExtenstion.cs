f1fb5c4ec8c5f42def7aeeb19cb23cf3c27bbd41
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 10:03:36 +0100

    Count the number of words

--- extension_method/StringExtenstion.cs
+++ extension_method/StringExtenstion.cs
@@ -4,6 +4,11 @@ namespace Extensions
 {
     public static class StringExtension
     {
+        /// <summary>
+        /// Count the number of words in a string
+        /// </summary>
+        /// <param name="text">The text to be counted</param>
+        /// <returns>The number of words.</returns>
         public static int CountWords(this string text)
         {
             return text.Split(' ').Length;
