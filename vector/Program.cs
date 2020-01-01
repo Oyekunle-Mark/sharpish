13e27d83aedfdfb11787a21dc0ce0b941564b6fd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 09:41:33 +0100

    Fixed naming

--- vector/Program.cs
+++ vector/Program.cs
@@ -9,11 +9,11 @@ namespace vector
             Vector v1 = new Vector(2, 3, 2);
             Vector v2 = new Vector(3, 5, 9);
 
-            Vector newVector = v1 + v2;
-            newVector.PrintVector();
+            Vector plusVector = v1 + v2;
+            plusVector.PrintVector();
 
-            Vector newVector = v1 - v2;
-            newVector.PrintVector();
+            Vector minusVector = v1 - v2;
+            minusVector.PrintVector();
         }
     }
 }
