b1b0ef4c439254c1ebfa484646580bcd53bbfbbd
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 09:58:30 +0100

    Returns the length of a text

--- extension_method/StringExtenstion.cs
+++ extension_method/StringExtenstion.cs
@@ -4,6 +4,9 @@ namespace Extensions
 {
     public static class StringExtension
     {
-
+        public static int CountWords(this string text)
+        {
+            return text.Split(' ').Length;
+        }
     }
 }
