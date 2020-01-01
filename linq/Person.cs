b35adfc008f4fd12570ec58b6dbb7bf9b32f90f9
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-01-01 10:32:13 +0100

    Prints person

--- linq/Person.cs
+++ linq/Person.cs
@@ -7,5 +7,7 @@ namespace linq
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public int Age { get; set; }
+
+        public void PrintPerson() => Console.WriteLine($"{Firstname}, {LastName}");
     }
 }
